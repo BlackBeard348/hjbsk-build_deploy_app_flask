e680fab668b98a780b8cc219ffbe39a9f0e0df16
BlackBeard348 <cmorgan348@outlook.com>
2025-07-13 09:11:37 +0800

    second commit

---
+++ Maths/mathematics.py
@@ -0,0 +1,11 @@
+def summation(a, b):
+    results = a + b
+    return results
+
+def subtraction(a, b):
+    results = a - b
+    return results 
+
+def multiplication(a, b):
+    results = a * b
+    return results 
